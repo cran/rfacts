a3e4f2c970de44a56a06089fd30ffaf30ce85b5a
William Michael Landau <will.landau@gmail.com>
2021-01-04 14:50:02 +0000

    version 0.1.0

--- NEWS.md
+++ NEWS.md
@@ -1,8 +1,13 @@
+# Version 0.1.0
+
+* Add new function `read_facts()` to read arbitrary XML fields of FACTS files.
+* Add new function `write_facts()` to programmatically generate modified copies of FACTS files.
+
 # Version 0.0.2
 
 * Mention CRAN in docs.
 * Use single quotes to reference software names in the `DESCRIPTION`.
-* Avoid `\dontrun{}` in examples. Instead, check if the required system dependencies are configured. Most of the examples need to be skipped because `rfacts` relies on system dependencies that are proprietary and not publicly available. To obtain the system dependencies, contact Berry Consultants: <https://www.berryconsultants.com/software>.
+* Avoid `\dontrun{}` in examples. Instead, check if the required system dependencies are configured. Most of the examples need to be skipped because `rfacts` relies on system dependencies that are proprietary and not publicly available. To obtain the system dependencies, contact Berry Consultants: <https://www.berryconsultants.com/software/>.
 * Always write to temp files in the README, examples, tests, and vignettes.
 
 # Version 0.0.1
---
+++ inst/doc/generate.R
@@ -0,0 +1,58 @@
+## ---- include = FALSE---------------------------------------------------------
+knitr::opts_chunk$set(collapse = TRUE, comment = "#>")
+knitr::opts_knit$set(root.dir = fs::dir_create(tempfile()))
+library(dplyr)
+library(rfacts)
+library(tibble)
+
+## -----------------------------------------------------------------------------
+library(dplyr)
+library(rfacts)
+library(tibble)
+tmp <- file.copy(get_facts_file_example("contin.facts"), getwd())
+facts_file <- "contin.facts"
+
+## -----------------------------------------------------------------------------
+field_subjects <- tibble(
+  field = "my_subjects",        # custom name the user can make up
+  type = "NucleusParameterSet", # "type" attribute of the <parameterSets> tag
+  set = "nucleus",              # "name" attribute of the <parameterSet> tag
+  property = "max_subjects"     # "name" attribute of the <property> tag
+)
+
+## -----------------------------------------------------------------------------
+field_vsr <- data.frame(
+  field = "my_vsr",                 
+  type = "EfficacyParameterSet",      
+  set = "resp2",                      
+  property = "true_endpoint_response" 
+)
+
+## -----------------------------------------------------------------------------
+fields <- bind_rows(field_subjects, field_vsr)
+fields
+
+## -----------------------------------------------------------------------------
+values <- tibble(
+  facts_file = facts_file,
+  my_subjects = c(1000, 2000),
+  my_vsr = list(c(15, 50), c(25, 75))
+)
+values
+
+## -----------------------------------------------------------------------------
+write_facts(fields = fields, values = values)
+list.files("_facts")
+
+## -----------------------------------------------------------------------------
+unlink("_facts", recursive = TRUE)
+values$output <- c("small.facts", "large.facts")
+write_facts(fields = fields, values = values)
+
+## -----------------------------------------------------------------------------
+read_facts(facts_file = facts_file, fields = fields)
+
+read_facts(facts_file = "small.facts", fields = fields)
+
+read_facts(facts_file = "large.facts", fields = fields)
+
